598d4a567875d8acbd04299961ad350f4ea4cfca
Christian Engel <christian.engel@wearekiss.com>
2019-03-26 21:37:57 +0100

    Expose new hook and SSR methods

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import A from './Link';
 import useRedirect from './redirect';
+import {useQueryParams, setQueryParams, getQueryParams} from "./queryParams";
 import {
 	navigate,
-	useRoutes
+	useRoutes,
+	setPath,
+	getPath,
 } from './router';
 
 export {
 	A,
 	useRedirect,
+	useQueryParams,
 	navigate,
 	useRoutes,
+	setPath,
+	getPath,
+	setQueryParams,
+	getQueryParams
 };
