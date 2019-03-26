1438d2b13763a67cd5ff533c309e5b44f983037d
Christian Engel <christian.engel@wearekiss.com>
2019-03-27 00:13:34 +0100

    Add interceptor module

---
+++ src/interceptor.js
@@ -0,0 +1,40 @@
+import React from 'react';
+let incrementalId = 1;
+
+const interceptors = [];
+
+export const interceptRoute = (previousRoute, nextRoute) => {
+	if(!interceptors.length){
+		return nextRoute;
+	}
+
+	return interceptors[0].handlerFunction(previousRoute, nextRoute);
+};
+
+const get = (componentId) => interceptors.find(obj => obj.componentId === componentId) || null;
+const remove = (componentId) => {
+	const index = interceptors.findIndex(obj => obj.componentId === componentId);
+	if(index !== -1){
+		interceptors.splice(index, 1);
+	}
+};
+
+export const useInterceptor = (handlerFunction) => {
+	const [componentId] = React.useState(incrementalId++);
+
+	let obj = get(componentId);
+
+	if(!obj){
+		obj = {
+			componentId,
+			stop: () => remove(componentId),
+			handlerFunction
+		};
+
+		interceptors.unshift(obj);
+	}
+
+	React.useEffect(() => () => obj.stop(), []);
+
+	return obj.stop;
+};
