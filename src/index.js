ec0eacd8582f40794956d604cf9faa8357843a93
Christian Engel <christian.engel@wearekiss.com>
2019-03-27 23:39:40 +0100

    Add useTitle() hook

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import A from './Link';
 import useRedirect from './redirect';
 import {useQueryParams, setQueryParams, getQueryParams} from "./queryParams";
 import {useInterceptor} from './interceptor';
+import {useTitle} from './title';
 import {
 	navigate,
 	useRoutes,
@@ -12,6 +13,7 @@ import {
 export {
 	A,
 	useRedirect,
+	useTitle,
 	useQueryParams,
 	useInterceptor,
 	navigate,
