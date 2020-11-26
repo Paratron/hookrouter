2f37c403b6e620402564d955a55e544be8ac3e09
Christian Engel <chrispy@parastudios.de>
2020-11-26 20:32:24 +0100

    Prepare url props extraction

--- src/routes.test.ts
+++ src/routes.test.ts
@@ -1,5 +1,29 @@
-import { createMemoryRouter, useRoutes } from "./routes";
+import { createMemoryRouter, getPathProps, useRoutes } from "./routes";
 import { mountHook } from "./_testHelper"
+
+describe("Helpers", () => {
+    test("getPathProps", () => {
+        const [regex, propNames] = getPathProps("/my/[test]/path.html");
+        // @ts-ignore
+        expect(regex.source).toBe(`\\/my\\/(.+?)\\/path.html`);
+        expect(propNames).toMatchObject(["test"]);
+
+        {
+            const [regex, propNames] = getPathProps("/my/[foo]/path-[bar].html");
+            // @ts-ignore
+            expect(regex.source).toBe(`\\/my\\/(.+?)\\/path-(.+?).html`);
+            expect(propNames).toMatchObject(["foo", "bar"]);
+        }
+
+        {
+            const [regex, propNames] = getPathProps("/path/without/props");
+            // @ts-ignore
+            expect(regex).toBe("/path/without/props");
+            expect(propNames).toMatchObject([]);
+        }
+    })
+});
+
 
 describe("useRoutes", () => {
 
@@ -11,7 +35,7 @@ describe("useRoutes", () => {
         };
 
         const router = createMemoryRouter();
-        const {act, root} = mountHook(() => useRoutes(routes), router);
+        const { act, root } = mountHook(() => useRoutes(routes), router);
         expect(routes["/"]).toHaveBeenCalledTimes(1);
         expect(root.toJSON()).toBe("root");
 
@@ -35,5 +59,18 @@ describe("useRoutes", () => {
         expect(routes["/"]).toHaveBeenCalledTimes(1);
         expect(routes["/route1"]).toHaveBeenCalledTimes(1);
         expect(root.toJSON()).toBe(null);
+    });
+
+    it("Extracts parameters from a url", () => {
+        let routeParams;
+
+        const routes = {
+            "/route/[slug]/id_[id].html": jest.fn((params) => routeParams = params)
+        };
+
+        const router = createMemoryRouter("/route/foo/id_bar.html");
+        const { act, root } = mountHook(() => useRoutes(routes), router);
+        expect(routes["/route/[slug]/id_[id].html"]).toHaveBeenCalled();
+        expect(routeParams).toMatchObject({ slug: "foo", id: "bar" });
     });
 });
--- src/routes.ts
+++ src/routes.ts
@@ -55,12 +55,58 @@ export const RouterProvider = ({ children, router }: RouterProviderProps) => {
     return React.createElement(routerContext.Provider, { value: router }, children);
 };
 
-export interface RoutesObject {
-    [key: string]: (props?: any) => any;
+const pathPropMatcher = /\[([a-zA-Z0-9]+?)]/g;
+
+export function getPathProps(pathMatcher: string): [RegExp | string, string[]] {
+    const pathProps = pathMatcher.match(pathPropMatcher);
+    if (!pathProps) {
+        return [pathMatcher, []];
+    }
+    const propNames: string[] = [];
+    let regexString = pathMatcher;
+    pathProps.forEach(propName => {
+        propNames.push(propName.substr(1, propName.length - 2));
+        regexString = regexString.replace(propName, `(.+?)`);
+    })
+    return [new RegExp(regexString), propNames];
 }
 
-export const useRoutes = (routes: RoutesObject) => {
+type PreparedRoute<T> = [RegExp | string, string[], RouteFunction<T>];
+
+function prepareRoutes<T>(receipt: RoutesObject<T>): PreparedRoute<T>[] {
+    return Object.entries(receipt).map(([matcher, routeFunction]) => [...getPathProps(matcher), routeFunction]);
+}
+
+function findRoute<T>(preparedRoutes: PreparedRoute<T>[], path: string): T | null {
+    for(let i = 0; i < preparedRoutes.length; i++){
+        const [matcher, propNames, routeFunction] = preparedRoutes[i];
+        if(typeof matcher === "string"){
+            if(path === matcher){
+                return routeFunction();
+            }
+            continue;
+        }
+        let props = matcher.exec(path);
+        if (props) {
+            props.shift();
+            return routeFunction(props.reduce((obj: {[key: string]: string}, value, i) => {
+                obj[propNames[i]] = value;
+                return obj;
+            }, {})) as T;
+        }
+    }
+    return null;
+}
+
+type RouteFunction<T> = (props?: any) => T;
+
+export interface RoutesObject<T> {
+    [key: string]: RouteFunction<T>;
+}
+
+export const useRoutes = <T = React.ReactNode>(routes: RoutesObject<T>): T | null => {
     const router = React.useContext(routerContext);
     const path = useSubscription(router);
-    return React.useMemo(() => routes[path] ? routes[path]() : null, [path])
+    const preparedRoutes = React.useMemo(() => prepareRoutes<T>(routes), [routes]);
+    return React.useMemo(() => findRoute<T>(preparedRoutes, path), [path, preparedRoutes])
 };
