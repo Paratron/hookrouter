2e3f31c40e7596d1d31829f8e13cf9e4758d2a39
jacklrs <github@rushsoft.de>
2021-05-10 13:23:47 +0200

    build: update devDependencies, make tests run again anf fix npm audits

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = function (api) {
 	const presets = [];
 
-	// Resolve ES2015+ only before publishing to NPM
-	if (process.env['NODE_ENV'] === 'production') {
-		presets.push('@babel/preset-env');
-	}
-
+	presets.push('@babel/preset-env');
 	presets.push('@babel/preset-react');
 
 	const plugins = [];
